35567eebdd91245f44a9258a49659412df6f2915
tvartiainen <46996237+tvartiainen@users.noreply.github.com>
2023-08-08 18:53:07 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,124 @@
+function skillsMember() {
+    var skills = [
+        {
+            name: 'HTML',
+            level: '90%'
+        },
+        {
+            name: 'CSS',
+            level: '80%'
+        },
+        {
+            name: 'Javascript',
+            level: '70%'
+        },
+        {
+            name: 'PHP',
+            level: '50%'
+        },
+        {
+            name: 'SQL',
+            level: '50%'
+        },
+        {
+            name: 'Python',
+            level: '50%'
+        },
+        {
+            name: 'C',
+            level: '50%'
+        },
+        {
+            name: 'C++',
+            level: '50%'
+        },
+        {
+            name: 'Java',
+            level: '50%'
+        },
+        {
+            name: 'C#',
+            level: '50%'
+        },
+        {
+            name: 'Ruby',
+            level: '50%'
+        },
+        {
+            name: 'Swift',
+            level: '50%'
+        },
+        {
+            name: 'Objective-C',
+            level: '50%'
+        },
+        {
+            name: 'Go',
+            level: '50%'
+        },
+        {
+            name: 'Perl',
+            level: '50%'
+        },
+        {
+            name: 'Scala',
+            level: '50%'
+        },
+        {
+            name: 'R',
+            level: '50%'
+        },
+        {
+            name: 'Haskell',
+            level: '50%'
+        },
+        {
+            name: 'Lua',
+            level: '50%'
+        },
+        {
+            name: 'Matlab',
+            level: '50%'
+        },
+        {
+            name: 'Kotlin',
+            level: '50%'
+        },
+        {
+            name: 'Rust',
+            level: '50%'
+        },
+        {
+            name: 'Dart',
+            level: '50%'
+        },
+        {
+            name: 'Julia',
+            level: '50%'
+        },
+        {
+            name: 'Elixir',
+            level: '50%'
+        },
+        {
+            name: 'Erlang',
+            level: '50%'
+        },
+        {
+            name: 'Clojure',
+            level: '50%'
+        },
+        {
+            name: 'Groovy',
+            level: '50%'
+        },
+        {
+            name: 'TypeScript',
+            level: '50%'
+        },
+        {
+            name: 'CoffeeScript',
+            level: '50%'
+        }
+        ]
+}
